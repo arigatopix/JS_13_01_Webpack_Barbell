9e5f29425c4e32271b8ce5c7e75851edb5290475
arigatopix <teeruch.k@gmail.com>
2019-04-06 12:43:42 +0700

    UI Create

--- src/app.js
+++ src/app.js
@@ -5,14 +5,12 @@ console.log(person.name);
  */
 
 //  ES2015 Module
+/* 
 import { person, sayHello } from './mymodule2';
-import greeting from './mymodule2';
-
-// * import all from module ไปวางไว้ใน mod ตอนเรียกใช้ต้องมี prefix เช่น mod.person.name
-// import * as mod from './mymodule2';
+import greeting from './mymodule2'; // * import without {} ต้องไปใข้ export default ไว้ก่อน
+// import * as mod from './mymodule2'; // * import all from module ไปวางไว้ใน mod ตอนเรียกใช้ต้องมี prefix เช่น mod.person.name
 
 console.log(person.name);
 console.log(sayHello());
-
-// * import without {} ต้องไปใข้ export default ไว้ก่อน
 console.log(greeting);
+ */
