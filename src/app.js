174dd003baad965378fc2f9c02e5b34c4beac602
arigatopix <teeruch.k@gmail.com>
2019-04-06 16:49:43 +0700

    version 1.0.0

--- src/app.js
+++ src/app.js
@@ -26,6 +26,15 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // listen for add post
 document.querySelector('.post-submit').addEventListener('click',submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click',deletePost);
+
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click',enableEdit);
+
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 // Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -39,20 +48,89 @@ function submitPost() {
   // get form value
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
-
+  const id = document.querySelector('#id').value;
+  
+  // Send data
   const data = {
     title,
     body // es2015+ syntax จะเหมือนกับ body : 'body' (es5)
   }
 
-  // Create Post
-  http.post('http://localhost:3000/posts',data)
-    .then(data => {
-      ui.showAlert('Post added', 'alert alert-success');
-      ui.clearFields();
-      // หลังจาก submit แล้วให้แสดงใน ui
-      getPosts();
-    })
-    .catch(err => console.log(err));
+  // Validate input
+  if (title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger');
+  } else {
+    if(id === '') {
+      // ถ้า id ไม่มีให้ Create Post
+      http.post('http://localhost:3000/posts',data)
+        .then(data => {
+          ui.showAlert('Post added', 'alert alert-success');
+          ui.clearFields();
+          // หลังจาก submit แล้วให้แสดงใน ui
+          getPosts();
+        })
+        .catch(err => console.log(err));
+      } else {
+        // Update post
+        // ถ้ากด edit post enable edit จะรับ ID ไว้
+        // หลังจากกด submit จะเข้า if state อันบน
+        http.put(`http://localhost:3000/posts/${id}`,data)
+          .then(data => {
+            ui.showAlert('Post edit', 'alert alert-success');
+            ui.changeFormState('add');
+            getPosts();
+          })
+          .catch(err => console.log(err));        
+      }  
+    }
+}
 
+// Delete Post
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains('delete')) {
+    // ? dataset.id ใช้คู่กับ custom data attr ของ HTML5
+    const id = e.target.parentElement.dataset.id;
+    if (confirm('Are you sure?')) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed', 'alert alert-success');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+}
+
+// Enable edit state
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    // ต้องการค่า textContent ใน title, body โดยดึงจาก id ที่เรากด edit
+    const body = e.target.parentElement.previousElementSibling.textContent;
+    // ใช้ Sibling คือพี่น้อง
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill form with current post ส่งให้ UI
+    ui.fillForm(data);
+    
+  }
+
+  e.preventDefault();
+}
+
+// Cancel Edit State
+function cancelEdit(e) {
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
+
+  e.preventDefault();
 }
--- src/ui.js
+++ src/ui.js
@@ -8,6 +8,7 @@ class UI {
     this.forState = 'add'; //default state
   }
 
+  // Show all post
   showPosts(posts){
     // เอาไปแสดงผล รับค่าจาก app.js (http.get)
     let output = '';
@@ -18,10 +19,11 @@ class UI {
           <div class="card-body">
             <h4 class="card-title">${post.title}</h4>
             <p class="card-text">${post.body}</p>
-            <a href="#" class="edit card-link data-id="${post.id}">
+            <a href="#" class="edit card-link" data-id="${post.id}">
               <i class="fa fa-pencil-alt"></i>
             </a>
-            <a href="#" class="delete card-link data-id="${post.id}">
+
+            <a href="#" class="delete card-link" data-id="${post.id}">
               <i class="fas fa-times"></i>
             </a>
           </div>
@@ -29,9 +31,11 @@ class UI {
       `;
     });
     // data-id เป็น custom attribut ที่ html5 สามารถทำได้
+    // ! โง่ ลืมดู "" ที่ class
     this.post.innerHTML = output;
   }
-  
+
+  // Show alert
   showAlert(message, className) {
     this.clearAlert();
 
@@ -54,6 +58,7 @@ class UI {
     },3000);
   }
 
+  // Cleart div alert
   clearAlert() {
     const currentAlert = document.querySelector('.alert');
     if(currentAlert) {
@@ -62,9 +67,65 @@ class UI {
     }
   }
 
+  // Clear all field after Submit
   clearFields() {
     this.titleInput.value = '';
     this.bodyInput.value = '';
+  }
+
+  // Fill form to edit
+  fillForm(data){
+    // * อย่าลืม .value นะเพราะรับมาจาก DOM
+    this.titleInput.value = data.title;
+    this.bodyInput.value = data.body;
+    this.idInput.value = data.id; // ที่ hidden ไว้ เพื่อบรรจุ id หลังจาก submit จะได้ส่ง id ปัจจุบันเข้าไปใน backend
+    
+    // State listen
+    this.changeFormState('edit');
+  }
+  
+  clearIdInput() {
+    // * อย่าลืม .value นะเพราะรับมาจาก DOM
+    this.idInput.value = '';
+  }
+
+  // Change the form State
+  changeFormState(type) {
+    if(type === 'edit') {
+      // เปลี่ยน text ในปุ่ม และเปลี่ยนสี
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+      // Create cancel button
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'));
+      
+      // Get parent 
+      const cardForm = document.querySelector('.card-form');
+      // Get element to insert before
+      const formEnd = document.querySelector('.form-end');
+
+      // แสดง button แค่อันเดียว
+      const buttonArr = Array.from(document.querySelectorAll('.post-cancel'))
+      if(buttonArr.length === 0) {
+        // Insert before (cancel button)
+        cardForm.insertBefore(button, formEnd);
+      }
+
+    } else {
+      this.postSubmit.textContent = 'Post It!';
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+      
+      // Remove cancel btn if it is there เอาปุ่ม cancel ออกถ้าเป้นปกติ
+      if(document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+      // Clear ID from hidden field
+      this.clearIdInput();
+      // Clear Test
+      this.clearFields()
+    }
   }
 }
 
